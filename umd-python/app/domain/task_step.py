2ccc1c8f0eefb01033852783400deeaba6900e89
CubeAI ★ 智立方 <50914939+cube-ai@users.noreply.github.com>
2021-08-12 11:45:17 +0800

    Delete umd-python directory

--- umd-python/app/domain/task_step.py
+++
@@ -1,9 +0,0 @@
-class TaskStep:
-    def __init__(self):
-        self.id = None
-        self.taskUuid = None
-        self.stepName = None
-        self.stepStatus = None
-        self.stepProgress = None
-        self.description = None
-        self.stepDate = None
